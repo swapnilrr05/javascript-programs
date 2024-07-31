1190ec4d88aa0879cbabffcf724a7d80230f5417
swapnilrr05 <swapnilrr05@gmail.com>
2024-07-31 17:27:02 +0530

    javascript-variables
    javascript-variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,5 @@
+console.log("My city is Sangli")
+console.log("My best name is Suyash")
+console.log("My country is INDIA")
+console.log("My school was SVRD Highschool, Shegaon")
+console.log("My college was ADCET Ashta")
---
+++ 01-variable.js
@@ -0,0 +1,16 @@
+// variable declaration
+var firstname;
+console.log(firstname);
+
+// variable initiallization
+firstname = "jenny"
+console.log(firstname);
+
+// variable desclaration and initialization
+var lastname = "gates"
+console.log(lastname);
+
+// variable updates
+firstname = "jennier";
+console.log(firstname);
+
