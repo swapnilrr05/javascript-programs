8d7cdc72326dec0f311c23464b2efc1a711dbc26
swapnilrr05 <swapnilrr05@gmail.com>
2024-08-02 21:00:36 +0530

    03-functionAssignment02
    03-functionAssignment02

---
+++ 03-functionAssignment02.js
@@ -0,0 +1,10 @@
+// Step 1: Define the function
+function bankDetails(bankName, accountNum, location, pinCode) {
+    // Step 2: Log the values in the function
+    console.log(`Bank Name: ${bankName}, Account Number: ${accountNum}, Location: ${location}, Pin Code: ${pinCode}`);
+}
+
+// Step 3: Invoke the function with different sets of arguments
+bankDetails("CITI Bank", 3456782345, 'Pune', 431202);
+bankDetails("Axis Bank", 7856782345, 'Mumbai', 441202);
+bankDetails("HDFC Bank", 8956782345, 'Pune', 631202, "Open");
