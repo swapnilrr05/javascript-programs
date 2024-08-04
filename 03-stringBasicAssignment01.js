bcafc4f9c4e8c8f8b8c49c41992d4ac8c346e940
swapnilrr05 <swapnilrr05@gmail.com>
2024-08-04 18:22:09 +0530

    03-stringBasicAssignment01
    03-stringBasicAssignment01

---
+++ 03-stringBasicAssignment01.js
@@ -0,0 +1,12 @@
+
+console.log('My dream company is "Tesla"');
+
+let hobby1 = "Reading,";
+let hobby2 = "Traveling,";
+let hobby3 = "Gaming";
+
+console.log("My Hobbies are: ", hobby1, hobby2, hobby3);
+
+let totalCharacters = hobby1.length + hobby2.length + hobby3.length;
+
+console.log("Total number of characters in all hobbies: ", totalCharacters);
