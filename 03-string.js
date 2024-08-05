d08373e404f56912d7f6333c049a9dd1af7f8f09
swapnilrr05 <swapnilrr05@gmail.com>
2024-08-05 20:05:24 +0530

    03-stringjs
    03-stringjs

---
+++ 03-string.js
@@ -0,0 +1,23 @@
+var greet = "Good Morning";
+console.log("Given string is: ");
+
+var greetLength= greet. length;
+console.log("Total number of charectors is:",greetLength);
+
+var charAt0 = greet.charAt(0);
+console.log("Charector at index 0 is: ", charAt0);
+
+var charAt5 = greet.charAt(5);
+console.log("Charector at index 5 is: ", charAt5);
+
+console.log("==== Last character ====");
+var LastChar=  greet.charAt(greet.length-1);
+console.log("charector at last index is:", LastChar);
+
+var upperCaseString= greet.toUpperCase();
+console.log("Result is:", upperCaseString);
+
+var result= greet.indexOf("o")
+console.log("the index of charector o is:",result);
+
+
