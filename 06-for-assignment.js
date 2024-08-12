39d05c3772f33740582fcb205b9f15c912286c26
swapnilrr05 <swapnilrr05@gmail.com>
2024-08-12 14:24:40 +0530

    06-for-assignment
    06-for-assignment

---
+++ 06-for-assignment.js
@@ -0,0 +1,40 @@
+console.log(`======= Print the numbers from 5 to 15 =========`);
+for (let i = 5; i<=15; i++) {
+    console.log(i);
+    
+    
+}
+console.log(`======= Print even numbers from 0 to 20 =========`);
+for (let index = 0; index <=20 ; index=index+2) {
+    console.log(index);
+    
+    
+}
+console.log(`======= Print the table of 5 =========`);
+for (let index = 5; index <=50 ; index=index+5) {
+    console.log(index);
+    
+    
+}
+console.log(`======= Print the numbers from 50 to 40========`);
+for (let index = 50; index >=40 ; index--) {
+    console.log(index);
+    
+    
+}
+console.log(`======= Print the table of 10 =========`);
+for (let index = 10; index <=100 ; index=index+10) {
+    console.log(index);
+    
+    
+}
+console.log(`======= Print the table of 10 in reverse order`);
+for (let index = 100; index >=10 ; index=index-10) {
+    console.log(index);
+    
+    
+}
+console.log(`======= Print odd numbers from 1 to 29 =========`);
+for (let index = 1; index <=29 ; index=index+2) {
+    console.log(index);
+}
